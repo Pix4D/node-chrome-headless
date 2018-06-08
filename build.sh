6067fe83abd1ed06c344e26de310942ceb793225
Olivier Amblet <olivier.amblet@pix4d.com>
2018-06-08 11:15:34 +0200

    fix: travis

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker --version
-docker build --no-cache -t pix4d/node-chrome-headless -f Dockerfile context
+docker build --no-cache -t node-chrome-headless -f Dockerfile context
 docker images
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-docker --version
-docker images
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag node-chrome-headless $DOCKER_TEAM/node-chrome-headless
-docker push $DOCKER_TEAM/node-chrome-headless
